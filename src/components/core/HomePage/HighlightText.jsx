e8c3d7721535a5b82d1cb49c40623b726644be6c
Raj Shah <rajpshah151@gmail.com>
2024-08-05 17:59:11 +0530

    added the code for buttons and highlighted text

---
+++ src/components/core/HomePage/HighlightText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HighlightText = ({text}) => {
+  return (
+    <span className='font-bold bg-gradient-to-br from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text'>
+        {text}
+    </span>
+  )
+}
+
+export default HighlightText
