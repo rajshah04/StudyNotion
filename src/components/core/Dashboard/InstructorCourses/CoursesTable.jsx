f11404f401640f0a1ac03e6efabe465127fb14d3
rajshah04 <rajpshah151@gmail.com>
2024-10-12 19:50:02 +0530

    added instructor's courses page

--- src/components/core/Dashboard/InstructorCourses/CoursesTable.jsx
+++ src/components/core/Dashboard/InstructorCourses/CoursesTable.jsx
@@ -6,6 +6,9 @@ import { useNavigate } from 'react-router-dom';
 import ConfirmationModal from '../../../common/ConfirmationModal';
 import { fetchInstructorCourses, deleteCourse } from '../../../../services/operations/courseDetailsAPI';
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import { MdDeleteForever } from 'react-icons/md';
+import { FaCheck, FaEdit } from "react-icons/fa";
+import { HiClock } from "react-icons/hi";
 
 const CoursesTable = ({courses, setCourses}) => {
 
@@ -15,6 +18,8 @@ const CoursesTable = ({courses, setCourses}) => {
 
     const [loading, setLoading] = useState(false) ;
     const [confirmationModal, setConfirmationModal] = useState(null) ;
+
+    const TRUNCATE_LENGTH = 30 ;
 
     const navigate = useNavigate() ;
 
@@ -57,19 +62,19 @@ const CoursesTable = ({courses, setCourses}) => {
 
     return (
         <div>
-            <Table>
+            <Table className='rounded-3xl border border-richblack-800'>
                 <Thead>
-                    <Tr className='flex gap-x-12 border-richblack-800 p-8'>
-                        <Th>
+                    <Tr className='flex gap-x-12 rounded-t-md border-b border-richblack-800 px-6 py-3'>
+                        <Th className='flex-1 text-left text-sm font-medium uppercase text-richblack-100'>
                             Courses
                         </Th>
-                        <Th>
+                        <Th className='text-left text-sm font-medium uppercase text-richblack-100'>
                             Duration
                         </Th>
-                        <Th>
+                        <Th className='text-left text-sm font-medium uppercase text-richblack-100'>
                             Price
                         </Th>
-                        <Th>
+                        <Th className='text-left text-sm font-medium uppercase text-richblack-100'>
                             Actions
                         </Th>
                     </Tr>
@@ -79,59 +84,64 @@ const CoursesTable = ({courses, setCourses}) => {
                     {
                         courses.length === 0 ? (
                             <Tr>
-                                <Td>
+                                <Td  className='py-10 text-center text-2xl font-medium text-richblack-100'>
                                     No Courses Found
                                 </Td>
                             </Tr>
                         ) : (
                             courses?.map((course) => (
-                                <Tr key={course._id} className='flex gap-x-12 border-richblack-800 p-8'>
+                                <Tr key={course._id} className='flex gap-x-12 border-b border-richblack-800 px-6 py-8'>
 
-                                    <Td className='flex gap-x-4' rowspan='1'>
+                                    <Td className='flex flex-1 gap-x-4'>
                                         <img src={course?.thumbnail} className='h-[150px] w-[220px] rounded-lg object-cover' loading='lazy' />
 
-                                        <div className='flex flex-col'>
-                                            <p>
+                                        <div className='flex flex-col items-start justify-between'>
+                                            <p className='text-lg font-semibold text-richblack-5'>
                                                 {course.courseName}
                                             </p>
-                                            <p>
-                                                {course.courseDescription}
+                                            <p  className='text-xs text-richblack-300'>
+                                                {
+                                                    course.courseDescription.split(" ").length > TRUNCATE_LENGTH ? 
+                                                    course.courseDescription.split(" ").slice(0, TRUNCATE_LENGTH).join(" ") + "..."
+                                                    : course.courseDescription
+                                                }
                                             </p>
-                                            <p>
-                                                Created :
+                                            <p className='text-xs text-white'>
+                                                Created on : {course.createdAt.slice(0, 10)}
                                             </p>
                                             {
-                                                // course.status === COURSE_STATUS.DRAFT ? (
-                                                    // add icon
-                                                    <p className='text-red-400'>
-                                                        DRAFTED
+                                                course.status === COURSE_STATUS.DRAFT ? (
+                                                    <p className="flex w-fit flex-row items-center gap-2 rounded-full bg-richblack-700 px-2 py-[2px] text-[12px] font-medium text-pink-100">
+                                                        <HiClock size={14} />
+                                                        Drafted
                                                     </p>
-                                                // ) : (
-                                                //     // add icon
-                                                //     <p className='text-yellow-50'>
-                                                //         PUBLISHED
-                                                //     </p>
-                                                // )
+                                                ) : (
+                                                    <p className="flex w-fit flex-row items-center gap-2 rounded-full bg-richblack-700 px-2 py-[2px] text-[12px] font-medium text-yellow-100">
+                                                        <div className="flex h-3 w-3 items-center justify-center rounded-full bg-yellow-100 text-richblack-700">
+                                                        <FaCheck size={8} />
+                                                        </div>
+                                                        Published
+                                                    </p>
+                                                )
                                             }
                                         </div>
                                     </Td>
 
-                                    <Td rowspan='1'>
+                                    <Td className='text-sm font-medium text-richblack-100'>
                                         2hr 30min
                                     </Td>
 
-                                    <Td>
-                                        {course.price}
+                                    <Td className='text-sm font-medium text-richblack-100'>
+                                        ₹{course.price}
                                     </Td>
 
-                                    <Td>
+                                    <Td className='text-sm font-medium text-richblack-100'>
                                         <button disabled={loading} 
                                         onClick={() => {
                                             navigate(`/dashboard/edit-course/${course._id}`)
-                                        }}
+                                        }} title='Edit' className='px-2 transition-all duration-200 hover:scale-110 hover:text-caribbeangreen-300'
                                         >
-                                            {/* add icon */}
-                                            Edit
+                                            <FaEdit className='text-xl' />
                                         </button>
                                         
                                         <button disabled={loading} 
@@ -144,10 +154,9 @@ const CoursesTable = ({courses, setCourses}) => {
                                                 btn1Handler: !loading ? () => handleCourseDelete(course._id) : () => {},
                                                 btn2Handler: !loading ? () =>setConfirmationModal(null) : () => {},
                                             })
-                                        }}
+                                        }} title='Delete' className='px-1 transition-all duration-200 hover:scale-110 hover:text-[#ff0000]'
                                         >
-                                            {/* add icon */}
-                                           Delete
+                                           <MdDeleteForever className='text-xl' />
                                         </button>
                                     </Td>
 
--- src/components/core/Dashboard/MyCourses/index.js
+++ src/components/core/Dashboard/MyCourses/index.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { fetchInstructorCourses } from '../../../../services/operations/courseDetailsAPI';
 import CommonBtn from '../../../common/CommonBtn';
 import CoursesTable from '../InstructorCourses/CoursesTable';
+import { FiPlusCircle } from 'react-icons/fi';
 
 const MyCourses = () => {
 
@@ -27,12 +28,12 @@ const MyCourses = () => {
     return (
         <div>
             
-            <div className='flex justify-between items-center'>
-                <h1>
+            <div className='mb-14 flex justify-between items-center'>
+                <h1 className='text-3xl font-medium text-richblack-5'>
                     My Courses
                 </h1>
 
-                <CommonBtn text="Add Course" children={"Add + icon"} onclick={() => navigate("/dashboard/add-course")} />                
+                <CommonBtn text="Add Course" children={<FiPlusCircle />} onclick={() => navigate("/dashboard/add-course")} />                
             </div>
 
             {
