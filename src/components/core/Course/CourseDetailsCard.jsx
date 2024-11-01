ba6b71a91b349c88b174b59f28278bfbf7eaf876
rajshah04 <rajpshah151@gmail.com>
2024-11-01 20:14:19 +0530

    added css

--- src/components/core/Course/CourseDetailsCard.jsx
+++ src/components/core/Course/CourseDetailsCard.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { ACCOUNT_TYPE } from '../../../utils/constants';
 import { addToCart } from '../../../slices/cartSlice';
+import { FaRegDotCircle, FaShareSquare } from 'react-icons/fa';
+import { GoDotFill } from 'react-icons/go';
 
 const CourseDetailsCard = ({course, setConfirmationModal, buyCourseHandler}) => {
 
@@ -49,56 +51,62 @@ const CourseDetailsCard = ({course, setConfirmationModal, buyCourseHandler}) => 
 
     return (
         <div className='flex flex-col gap-4 rounded-md bg-richblack-700 p-4 text-richblack-5'>
-            <img src={ThumbnailImage} alt='Thumbnail image' className='max-h-80 min-h-[180px] w-[400px] rounded-2xl overflow-hidden object-cover md:max-w-full' />
+            <img src={ThumbnailImage} alt='Thumbnail image' className='max-h-60 min-h-[180px] w-[400px] rounded-2xl overflow-hidden object-cover md:max-w-full' />
 
-            <div className='space-x-3 pb-4 text-3xl font-semibold'>
-                Rs. {CurrentPrice}
-            </div>
+            <div className='w-[350px] mx-auto'>            
 
-            <div className='flex flex-col gap-6'>
-                <button onClick={user && course?.studentsEnrolled.includes(user?._id) ? () => navigate("/dashboard/enrolled-courses") : buyCourseHandler} className='bg-yellow-50 w-fit text-richblack-900 cursor-pointer rounded-md px-[20px] py-[8px] font-semibold' >
-                    {
-                        user && course?.studentsEnrolled.includes(user?._id) ? "Go to Course" : "Buy Now"
-                    }
-                </button>
+                <div className='space-x-3 pb-4 text-3xl font-semibold'>
+                    Rs. {CurrentPrice}
+                </div>
 
-                {
-                    !course?.studentsEnrolled.includes(user?._id) &&
-                    <button onClick={addToCartHandler} className='w-fit cursor-pointer rounded-md bg-richblack-800 px-[20px] py-[8px] font-semibold text-richblack-5' >
-                        Add to Cart
+                <div className='flex flex-col gap-6'>
+                    <button onClick={user && course?.studentsEnrolled.includes(user?._id) ? () => navigate("/dashboard/enrolled-courses") : buyCourseHandler} className='bg-yellow-50 text-richblack-900 cursor-pointer rounded-md px-[20px] py-[8px] font-semibold hover:scale-105 transition-all duration-200' >
+                        {
+                            user && course?.studentsEnrolled.includes(user?._id) ? "Go to Course" : "Buy Now"
+                        }
                     </button>
-                }
-            </div>
 
-            <div>
-                <p className='pb-3 pt-6 text-center text-sm text-richblack-25'>
-                    30 Day Money Back GUARANTEE
-                </p>
-
-                <p className='my-2 text-xl font-semibold'>
-                    This course includes :
-                </p>
-
-                <div className='flex flex-col gap-3 text-sm text-caribbeangreen-100'>
                     {
-                        course?.instructions?.map((item, ind) => (
-                            <p key={ind} className='flex gap-2'>
-                                <span>
-                                    {item}
-                                </span>
-                            </p>
-                        ))
+                        !course?.studentsEnrolled.includes(user?._id) &&
+                        <button onClick={addToCartHandler} className='cursor-pointer rounded-md bg-richblack-800 px-[20px] py-[8px] font-semibold text-richblack-5 hover:scale-105 transition-all duration-200' >
+                            Add to Cart
+                        </button>
                     }
                 </div>
- 
-            </div>
 
-            {/* share */}
-            <div className='text-center'>
-                <button onClick={shareHandler} className='mx-auto flex items-center gap-2 p-6 text-yellow-50' >
-                    {/* add icon here */}
-                    Share
-                </button>
+                <div>
+                    <p className='pb-3 pt-6 text-center text-sm text-richblack-25'>
+                        30 Day Money Back GUARANTEE
+                    </p>
+
+                    <p className='my-2 text-xl font-semibold'>
+                        This course includes :
+                    </p>
+
+                    <div className='flex flex-col gap-3 text-sm text-caribbeangreen-100'>
+                        {
+                            course?.instructions?.map((item, ind) => (
+                                <p key={ind} className='flex items-center gap-x-2'>
+                                    <GoDotFill />
+
+                                    <span>
+                                        {item}
+                                    </span>
+                                </p>
+                            ))
+                        }
+                    </div>
+    
+                </div>
+
+                {/* share */}
+                <div className='text-center'>
+                    <button onClick={shareHandler} className='mx-auto flex items-center gap-2 p-6 text-yellow-50' >
+                        Share
+                        <FaShareSquare size={20} />
+                    </button>
+                </div>
+
             </div>
         </div>
     )
--- src/components/core/Dashboard/EnrolledCourses.jsx
+++ src/components/core/Dashboard/EnrolledCourses.jsx
@@ -1,76 +1,83 @@
 import ProgressBar from '@ramonak/react-progress-bar';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { getUserEnrolledCourses } from '../../../services/operations/profileAPI';
+import { useNavigate } from 'react-router-dom';
 
 
 const EnrolledCourses = () => {
 
     const { token } = useSelector((state) => state.auth) ;
-    const [enrolledCourses, setEnrolledCourses] = useState(null) ;
+    const [enrolledCourses, setEnrolledCourses] = useState([]) ;
 
-    const getEnrolledCourses = async() => {
-        try{
-            // function call to api to get enrolled courses details
-            // const response = await 
-            // setEnrolledCourses(response) ;
-        }catch(err){
-            console.log("Error in fetching enrolled courses", err) ;
-        }
-    }
+    const navigate = useNavigate() ;
 
     useEffect(() => {
+        const getEnrolledCourses = async() => {
+            try{
+                // function call to api to get enrolled courses details
+                const response = await getUserEnrolledCourses(token) ;
+    
+                console.log("User enrolled courses : ", response) ;
+    
+                setEnrolledCourses(response) ;
+            }catch(err){
+                console.log("Error in fetching enrolled courses", err) ;
+            }
+        }
+
         getEnrolledCourses() ;
-    },[])
+    }, [])
 
     return (
         <div>
             
-            <div>Enrolled Courses</div>
+            <div className='text-3xl text-richblack-50'>Enrolled Courses</div>
             {
                 !enrolledCourses ? (
                     <div className='w-full h-[75vh] grid place-items-center'>
                         <div className='custom-loader' />
                     </div>
                 ) : !enrolledCourses.length ? (
-                    <p>
+                    <p className='grid h-[10vh] w-full place-content-center text-richblack-5'>
                         You have not enrolled in any course yet
                     </p>
                 ) : (
-                    <div>   
-                        <div>
-                            <p>
+                    <div className='my-8 text-richblack-5'>   
+                        <div className='flex rounded-t-xl bg-richblack-500'>
+                            <p className='w-[45%] px-6 py-3'>
                                 Course Name
                             </p>
-                            <p>
+                            <p className='w-1/4 px-3 py-3'>
                                 Duration
                             </p>
-                            <p>
+                            <p className='flex-1 px-2 py-3'>
                                 Progress
                             </p>
                         </div>
 
                         {/* Course cards */}
                         {
-                            enrolledCourses.map((course, index) => (
-                                <div>
-                                    <div>
-                                        <img src={course.thumbnail} alt={course?.courseName} />
+                            enrolledCourses.map((course, index, arr) => (
+                                <div key={index} className={`flex items-center border border-richblack-700 ${ index === arr.length - 1 ? "rounded-b-lg" : "rounded-none" } hover:border-richblack-300`} >
+                                    <div className='flex w-[45%] cursor-pointer items-center gap-4 px-5 py-3' onClick={() => { navigate(`/view-course/${course?._id}/section/${course.courseContent?.[0]?._id}/sub-section/${course.courseContent?.[0]?.subSection?.[0]?._id}`)}} >
+                                        <img src={course.thumbnail} alt={course?.courseName} className='h-14 w-14 rounded-lg object-cover' />
 
-                                        <div>
-                                            <p>
+                                        <div className='flex max-w-xs flex-col gap-2'>
+                                            <p className='font-semibold'>
                                                 {course.courseName}
                                             </p>
-                                            <p>
-                                                {course.courseName}
-                                            </p>CourseDecription
+                                            <p className='text-xs text-richblack-300'>
+                                                {course.courseDescription.length > 50 ? `${course.courseDescription.slice(0, 50)}...` : course.courseDescription}
+                                            </p>
                                         </div>
                                     </div>
 
-                                    <div>
-                                        {course?.totalDuration}
+                                    <div className='w-1/4 px-3 py-3'>
+                                        {course.totalDuration || 0}
                                     </div>
 
-                                    <div>
+                                    <div className='flex w-1/5 flex-col gap-2 px-3 py-3'>
                                         <p>
                                             Progress: {course.progressPercentage || 0}%
                                         </p>
