734e3e62bba3ed696a60f84b10bb89a7a8db5508
rajshah04 <rajpshah151@gmail.com>
2024-11-02 18:12:07 +0530

    added date formatter for course created date-time

---
+++ src/utils/dateFormatter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const dateFormatter = (createdAt) => {
+    // console.log("Created at : ", createdAt) ;
+    const options = { year: "numeric", month: "long", day: "numeric" } ;
+    const date = new Date(createdAt) ;
+    // console.log("Date : ", date) ;
+    const formattedDate = date.toLocaleString('en-US', options) ;
+
+    const hour = date.getHours() ;
+    const minutes = date.getMinutes() ;
+    const period = hour >= 12 ? "P.M." : "A.M." ;
+    const formattedTime = `${hour % 12}:${minutes.toString().padStart(2, "0")} ${period}` ;
+
+    // console.log(`${formattedDate} | ${formattedTime}`)
+
+    return `${formattedDate} | ${formattedTime}` ;
+}
+
+export default dateFormatter
