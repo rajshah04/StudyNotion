0fdf73437a3e12d2d7e4fcb0feca7cfd1a392025
rajshah04 <rajpshah151@gmail.com>
2024-11-20 02:59:42 +0530

    styled course review modal

--- src/components/core/ViewCourse/CourseReviewModal.jsx
+++ src/components/core/ViewCourse/CourseReviewModal.jsx
@@ -5,13 +5,14 @@ import ReactStars from 'react-rating-stars-component';
 import CommonBtn from '../../common/CommonBtn';
 import { createRating } from '../../../services/operations/courseDetailsAPI';
 import { useParams } from 'react-router-dom';
+import { ImCross } from 'react-icons/im';
 
 const CourseReviewModal = ({setReviewModal}) => {
 
     const { user } = useSelector((state) => state.profile) ;
     const { token } = useSelector((state) => state.auth) ;
 
-    const courseId = useParams() ;
+    const { courseId } = useParams() ;
 
     const {
         register,
@@ -22,7 +23,7 @@ const CourseReviewModal = ({setReviewModal}) => {
 
     useEffect(() => {
         setValue("courseExperience", "") ;
-        setValue("courseRating", "") ;
+        setValue("courseRating", 0) ;
     }, []) ;
 
     const ratingChanged = (newRating) => {
@@ -30,71 +31,106 @@ const CourseReviewModal = ({setReviewModal}) => {
     }
 
     const onSubmit = async(data) => {
+        console.log("Course id : ", courseId) ;
+
         await createRating({courseId: courseId, rating: data.courseRating, review: data.courseExperience}, token) ;
 
         setReviewModal(false) ;
     }
 
+    const secondExample = {
+        size: 50,
+        count: 10,
+        color: "black",
+        activeColor: "red",
+        value: 7.5,
+        a11y: true,
+        isHalf: true,
+        emptyIcon: <i className="far fa-star" />,
+        halfIcon: <i className="fa fa-star-half-alt" />,
+        filledIcon: <i className="fa fa-star" />,
+        onChange: newValue => {
+            console.log(`Example 2: new value is ${newValue}`);
+        }
+    };
+
     return (
-        <div>
-            {/* modal header */}
-            <div>
-                <p>
-                    Add Review
-                </p>
+        <div className='fixed inset-0 z-[1000] !mt-0 grid h-screen w-screen place-items-center overflow-auto bg-white bg-opacity-10 backdrop-blur-sm'>
+        
+            <div className='my-10 w-11/12 max-w-[700px] rounded-lg border border-richblack-400 bg-richblack-800'>
+                {/* modal header */}
+                <div className='flex items-center justify-between rounded-t-lg bg-richblack-700 p-5'>
+                    <p className='text-xl font-semibold text-richblack-5'>
+                        Add Review
+                    </p>
 
-                <button onClick={setReviewModal(false)}>
-                    {/* add X icon here */}
-                    x
-                </button>
-            </div>
-
-            {/* modal body */}
-            <div>
-                <div>
-                    <img src={user?.image} alt={`${user?.firstName}-profile`} className='aspect-square w-[50px] rounded-full object-cover' />
-
-                    <div>
-                        <p>
-                            {user?.firstName} {user?.lastName}
-                        </p>
-
-                        <p>
-                            Posting Publicly
-                        </p>
-                    </div>
+                    <button onClick={() => setReviewModal(false)}>
+                        <ImCross className='text-2xl text-richblack-5' />
+                    </button>
                 </div>
 
-                <form onSubmit={handleSubmit(onSubmit)} className='mt-6 flex flex-col items-center'>
+                {/* modal body */}
+                <div className='p-6'>
+                    <div className='flex items-center w-11/12 mx-auto gap-x-4'>
+                        <img src={user?.image} alt={`${user?.firstName}-profile`} className='aspect-square w-[60px] rounded-full object-cover' />
 
-                    <ReactStars count={5} onChange={ratingChanged} size={24} activeColor="#ffd700" />
+                        <div>
+                            <p className='font-semibold text-lg text-richblack-5'>
+                                {user?.firstName} {user?.lastName}
+                            </p>
 
-                    <div>
-                        <label htmlFor='courseExperience'>
-                            Add Your Experience
-                            {/* add *   */}
-                        </label>
-                        <textarea name="courseExperience" id="courseExperience" placeholder='Add your experience here' className='rounded-lg bg-richblack-700 p-3 text-[16px] leading-[24px] text-richblack-5 shadow-[0_1px_0_0] shadow-white/50 placeholder:text-richblack-400 focus:outline-none min-h-[130px] w-full' {...register("courseExperience", {required: true})} />
-                        {
-                            errors.courseExperience && (
-                                <span>
-                                    Please add your experience
-                                </span>
-                            )
-                        }
-
+                            <p className='text-sm text-richblack-100'>
+                                Posting Publicly
+                            </p>
+                        </div>
                     </div>
 
-                    <div>
-                        <button onClick={() => setReviewModal(false)}>
-                            Cancel
-                        </button>
+                    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center'>
 
-                        <CommonBtn text="Save" />
-                    </div>
+                        {/* <ReactStars count={5} onChange={ratingChanged} size="50" activeColor="#ffd700" /> */}
 
-                </form>
+                        {/* <ReactStars {...secondExample} /> */}
+
+                        <ReactStars
+                            count={5}
+                            onChange={ratingChanged}
+                            size={50}
+                            a11y={true}
+                            isHalf={true}
+                            emptyIcon={<i className="far fa-star"></i>}
+                            halfIcon={<i className="fa fa-star-half-alt"></i>}
+                            fullIcon={<i className="fa fa-star"></i>}
+                            activeColor="#ffd700"
+                        />
+
+                        <div className='flex flex-col w-11/12 mt-1 space-y-2'>
+                            <label htmlFor='courseExperience' className='text-sm text-richblack-5 px-3'>
+                                Add Your Experience
+                                <sup className='text-red-600'>*</sup>
+                            </label>
+                            <textarea name="courseExperience" id="courseExperience" placeholder='Add your experience here' className='rounded-lg bg-richblack-700 p-3 text-[16px] leading-[24px] text-richblack-5 shadow-[0_1px_0_0] shadow-white/50 placeholder:text-richblack-400 focus:outline-none min-h-[130px] w-full' {...register("courseExperience", {required: true})} />
+                            {
+                                errors.courseExperience && (
+                                    <span className='ml-2 text-xs tracking-wide text-red-600'>
+                                        Please add your experience
+                                    </span>
+                                )
+                            }
+
+                        </div>
+
+                        <div className='mt-6 flex w-11/12 justify-end gap-x-4'>
+                            <button onClick={() => setReviewModal(false)} className='flex cursor-pointer items-center gap-x-2 rounded-md bg-richblack-300 py-[8px] px-[20px] font-semibold text-richblack-900 hover:scale-110 transition-all duration-200' >
+                                Cancel
+                            </button>
+
+                            <CommonBtn text="Save" customClasses={'hover:scale-110 transition-all duration-200'} />
+                        </div>
+
+                    </form>
+                </div>
             </div>
+        
         </div>
     )
 }
