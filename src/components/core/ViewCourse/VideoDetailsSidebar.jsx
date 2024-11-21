ccff0b95a7c3bfbb42c7943a9dcdde340a46da8e
rajshah04 <rajpshah151@gmail.com>
2024-11-21 17:29:22 +0530

    added styles to video detail sidebar

--- src/components/core/ViewCourse/VideoDetailsSidebar.jsx
+++ src/components/core/ViewCourse/VideoDetailsSidebar.jsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import CommonBtn from '../../common/CommonBtn';
 import { useParams } from 'react-router-dom';
+import { IoChevronBackOutline } from 'react-icons/io5';
+import { MdOutlineArrowDropDown } from 'react-icons/md';
 
 const VideoDetailsSidebar = ({ setReviewModal }) => {
 
@@ -12,7 +14,7 @@ const VideoDetailsSidebar = ({ setReviewModal }) => {
     const navigate = useNavigate() ;
     const location = useLocation() ;
 
-    const { sectionId, subSectionId} = useParams() ;
+    const { sectionId, subSectionId } = useParams() ;
 
     const {
         courseSectionData,
@@ -43,15 +45,13 @@ const VideoDetailsSidebar = ({ setReviewModal }) => {
 
     return (
         <>
-            <div>
+            <div className='flex h-[calc(100vh-3.5rem)] w-[270px] max-w-[320px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800'>
                 {/* for buttons and headings */}
-                <div>
+                <div className='mx-5 flex flex-col items-start justify-between gap-2 gap-y-4 border-b border-richblack-600 py-5 text-lg font-bold text-richblack-25'>
                     {/* for buttons */}
-                    <div>
-                        <div onClick={() => navigate("/dashboard/enrolled-courses")}>
-                            {/* add icon */}
-                            Back
-
+                    <div className='flex w-full items-center justify-between'>
+                        <div onClick={() => navigate("/dashboard/enrolled-courses")} className='flex h-[35px] w-[35px] items-center justify-center rounded-full bg-richblack-100 p-1 text-richblack-700 hover:scale-90 cursor-pointer' title='Back'>
+                            <IoChevronBackOutline size={35} />
                         </div>
 
                         <div>
@@ -60,50 +60,51 @@ const VideoDetailsSidebar = ({ setReviewModal }) => {
                     </div>
 
                     {/* for heading */}
-                    <div>
+                    <div className='flex flex-col'>
                         <p>
                             {courseEntireData?.courseName}
                         </p>
 
-                        <p>
+                        <p className='text-richblack-500 font-semibold text-sm'>
                             {completedLectures?.length} / {totalNoOfLectures}
                         </p>
                     </div>
                 </div>
 
                 {/* sections and subsections */}
-                <div>
+                <div className='h-[calc(100vh - 5rem)] overflow-y-auto'>
                     {
                         courseSectionData.map((section, index) => (
-                            <div onClick={() => setActiveStatus(section?._id)} key={index} >
+                            <div onClick={() => setActiveStatus(section?._id)} key={index} className='mt-2 cursor-pointer text-sm text-richblack-5' >
 
                                 {/* section */}
-                                <div>
-                                    <div>
+                                <div className='flex flex-row justify-between items-center bg-richblack-600 px-5 py-4'>
+                                    <div className='w-[70%] font-semibold'>
                                         {section?.sectionName}
                                     </div>
 
                                     {/* add arrow icon here and handle rotate logic */}
+                                    <span className={`${activeStatus === section?._id ? "rotate-180" : "rotate-0"} duration-500 transition-all`}>
+                                        <MdOutlineArrowDropDown size={25} />
+                                    </span>
                                 </div>
 
                                 {/* subSections */}
                                 <div>
                                     {
                                         activeStatus === section?._id  && (
-                                            <div>
+                                            <div className='transition-[height] duration-500 ease-in-out'>
                                                 {
                                                     section.subSection.map((topic, index) => (
-                                                        <div className={`flex gap-4 p-4 ${videoBarActive === topic._id ? "bg-yellow-200 text-richblack-900" : "bg-richblack-900 text-richblack-5"}`} key={index} 
+                                                        <div className={`flex gap-4 p-4 ${videoBarActive === topic._id ? "bg-yellow-100 text-richblack-800 font-semibold" : "hover:bg-richblack-900"}`} key={index} 
                                                         onClick={() =>{
                                                             navigate(`/view-course/${courseEntireData?._id}/section/${section._id}/sub-section/${topic?._id}`) ;
 
                                                             setVideoBarActive(topic?._id) ;
                                                         }} >
                                                             <input type="checkbox" checked={completedLectures.includes(topic?._id)} onChange={() => {}} />
-
-                                                            <span>
-                                                                {topic.title}
-                                                            </span>
+                                                                
+                                                            {topic.title}                                                            
                                                         </div>
                                                     ))
                                                 }
