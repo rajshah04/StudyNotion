a79c171146548aa3ab15bbe345761df1817e106b
rajshah04 <rajpshah151@gmail.com>
2024-11-28 17:42:12 +0530

    added styling to the instructor dashboard

--- src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
+++ src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { fetchInstructorCourses } from "../../../../services/operations/courseDetailsAPI";
 import { getInstructorData } from '../../../../services/operations/profileAPI';
 import { Link } from 'react-router-dom';
+import InstructorChart from './InstructorChart';
 
 const Instructor = () => {
 
@@ -43,100 +44,136 @@ const Instructor = () => {
     return (
         <div className='text-richblack-5'>
             <div>
-                <h1>
-                    Hi {user?.firstName} 👋
+                <h1 className='font-bold text-3xl mb-4'>
+                    Hi {user?.firstName}👋
                 </h1>
 
-                <p>
+                <p className='text-sm text-richblack-300 my-4 font-medium'>
                     Let's start something new
                 </p>
             </div>
 
             {
                 loading ? 
-                (<div>
-                    <div>
-                        Loading    
+                (<div className='grid place-items-center w-full min-h-full'>
+                    <div className='chart-loader' >      
+
                     </div>    
                 </div>)
                 : courses.length > 0
                 ? (
                     <div>
-                        <div>
-                            <div>
-                              
 
-                                <div>
-                                    <p>
-                                        Statistics
-                                    </p>
-
-                                    <div>
-                                        <p>
-                                            Total Courses
-                                        </p>
-                                        <p>
-                                            {courses.length}
-                                        </p>
-                                    </div>
-
-                                    <div>
-                                        <p>
-                                            Total Students
-                                        </p>
-                                        <p>
-                                            {totalStudents}
-                                        </p>
-                                    </div>
-
-                                    <div>
-                                        <p>
-                                            Total Income
-                                        </p>
-                                        <p>
-                                            {totalAmount}
-                                        </p>
-                                    </div>
+                        <div className='my-4 flex min-h-fit space-x-4'>
+                            {totalAmount > 0 || totalStudents > 0 ? (
+                                <div className='w-[75%] rounded-md bg-richblack-800 p-6'>
+                                    <InstructorChart courses={instructorData} />
                                 </div>
-                            </div>    
-                        </div>
+                            ) : (
+                                <div className="flex-1 rounded-md bg-richblack-800 p-6">
+                                    <p className="text-xl font-semibold">
+                                        Visualize
+                                    </p>
+                                    <p className="mt-4 text-xl font-medium0">
+                                        Not Enough Data To Visualize
+                                    </p>
+                                </div>
+                            )}
 
-                        <div>
+                            <div className='w-[25%] flex flex-col items-start p-6 gap-y-4 rounded-md bg-richblack-800'>
+                                <p className='text-xl font-semibold'>
+                                    Statistics
+                                </p>
+
+                                <div className='space-y-1'>
+                                    <p className='text-lg text-richblack-300'>
+                                        Total Courses
+                                    </p>
+                                    <p className='text-2xl text-richblack-5 font-bold'>
+                                        {courses.length}
+                                    </p>
+                                </div>
+
+                                <div className='space-y-1'>
+                                    <p className='text-lg text-richblack-300'>
+                                        Total Students
+                                    </p>
+                                    <p className='text-2xl text-richblack-5 font-bold'>
+                                        {totalStudents}
+                                    </p>
+                                </div>
+
+                                <div className='space-y-1'>
+                                    <p className='text-lg text-richblack-300'>
+                                        Total Income
+                                    </p>
+                                    <p className='text-2xl text-richblack-5 font-bold'>
+                                        Rs. {totalAmount}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>    
+
+                        <div className='bg-richblack-800 rounded-md p-6'>
                             {/* render 3 courses */}
-                            <div>
-                                <p>
+                            <div className='flex justify-between'>
+                                <p className='text-xl font-semibold'>
                                     Your Courses
                                 </p>
 
                                 <Link to="/dashboard/my-courses">
-                                    <p>
+                                    <p className='text-sm font-semibold text-yellow-50'>
                                         View all
                                     </p>
                                 </Link>
                             </div>
 
-                            <div className='flex gap-2 w-full'>
+                            <div className='flex items-start space-x-6 my-4'>
                                 {
                                     courses.slice(0, 3).map((course) => (
-                                        <div>
-                                            <img src={course.thumbnail} alt="" className='h-[300px]' />
+                                        <div key={course._id} className='w-[33%]'>
+                                            <img src={course.thumbnail} alt="" className='h-[201px] w-full rounded-md object-cover' />
 
-                                            <div>
-                                                <p>
+                                            <div className='mt-3 w-full'>
+                                                <p className='text-sm font-medium text-richblack-50'>
                                                     {course.courseName}
                                                 </p>
 
-                                                <div>
-                                                    <p>
-                                                        {course.studentsEnrolled.length} students
+                                                <div className='mt-1 flex items-center space-x-2'>
+                                                    <p className='text-xs font-medium text-richblack-300'>
+                                                        {
+                                                            course.studentsEnrolled.length > 1
+                                                            ? (
+                                                                <p>
+                                                                    {course.studentsEnrolled.length} Students Enrolled
+                                                                </p>
+                                                            )
+                                                            : (
+                                                                <p>
+                                                                    {course.studentsEnrolled.length} Student Enrolled
+                                                                </p>
+                                                            )
+                                                        } 
                                                     </p>
 
-                                                    <p>
+                                                    <p className='text-xs font-medium text-richblack-300'>
                                                         |
                                                     </p>
 
-                                                    <p>
-                                                        Rs. {course.price}
+                                                    <p className='text-xs font-medium text-richblack-300'>
+                                                        {
+                                                            course.price === 0 
+                                                            ? (
+                                                                <p>
+                                                                    Free
+                                                                </p>
+                                                            ) 
+                                                            : (
+                                                                <p>
+                                                                    Rs. {course.price}
+                                                                </p>
+                                                            ) 
+                                                        }
                                                     </p>
                                                 </div>
                                             </div>
@@ -148,13 +185,15 @@ const Instructor = () => {
                     </div>
                 )
                 : (
-                    <div>
-                        <p>
+                    <div className='mt-20 rounded-md bg-richblack-800 p-6 py-20'>
+                        <p className='text-center text-2xl font-bold'>
                             You have not created any courses yet
                         </p>
 
                         <Link to={"/dashboard/add-course"}>
-                            Create a Course
+                            <p className='mt-1 text-center text-lg font-semibold text-yellow-50'>
+                                Create a Course
+                            </p>
                         </Link>
                     </div>
                 )
